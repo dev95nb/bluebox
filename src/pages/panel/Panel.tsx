192cd48864fe27d9d1282245d27843cb76d59256
Dung Nguyen <dungok195@gmail.com>
2023-04-05 07:39:22 +0700

    update code

--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -1,101 +1,63 @@
 import React from 'react'
 import '@pages/panel/Panel.css'
 import {
-  Button,
-  Grid,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
+  Container,
+  Stack,
+  TextField,
+  FormGroup,
+  ButtonGroup,
+  FormControl,
+  Select,
+  MenuItem,
+  Box,
 } from '@mui/material'
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein }
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-]
+import Button from '@mui/material/Button'
+import DoDisturbAltIcon from '@mui/icons-material/DoDisturbAlt'
+import StopCircleIcon from '@mui/icons-material/StopCircle'
 
 const Panel: React.FC = () => {
   return (
-    <div className="container">
-      <Grid container spacing={2}>
-        <Grid xs={8}>
-          <h1>Test</h1>
-        </Grid>
-        <Grid xs={4}>
-          <h2>OKOK</h2>
-        </Grid>
-      </Grid>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+    <>
+      <Container maxWidth={false} disableGutters>
+        <Stack direction={'row'} spacing={2}>
+          <Button
+            color="error"
+            variant="contained"
+            size="small"
+            startIcon={<StopCircleIcon />}
+          >
+            Stop
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            startIcon={<DoDisturbAltIcon />}
+          >
+            Clear
+          </Button>
+        </Stack>
+        <Stack direction={'row'} spacing={2} sx={{mt: 2}}>
+          <TextField placeholder="Search" size="small" variant="outlined" />
+          <Button variant="contained" size="small">
+            Filter
+          </Button>
+
+          <FormControl sx={{ minWidth: 120 }} size="small">
+            <Select defaultValue={10}>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+
+          <ButtonGroup variant="contained" size="small" >
+            <Button>GET</Button>
+            <Button>PUT</Button>
+            <Button>POST</Button>
+          </ButtonGroup>
+        </Stack>
+      </Container>
+    </>
   )
 }
 
